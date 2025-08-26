04ae7a78e627bcc9dcb2f46bdb64669a0a742755
Hamedblue1381 <hamed.balanar@gmail.com>
2025-08-26 15:33:38 +0330

    feat: add client call to plugin

--- client/index.ts
+++ client/index.ts
@@ -14,3 +14,24 @@ const createClient = async () => {
 }
 
 const { client, waterMarker } = await createClient()
+const arrayBuffer = await Bun.file("debug.jpg").arrayBuffer();
+const imageBuffer = new Uint8Array(arrayBuffer);
+
+const args = { text: "test", location: "southwest", image: imageBuffer } as Args
+
+const estimatedSize =
+    imageBuffer.length +
+    Buffer.byteLength(args.text, "ascii") +
+    Buffer.byteLength(args.location, "ascii") +
+    64;
+
+const buffer = Sia.alloc(estimatedSize);
+
+console.log('Sending request to worker...');
+await waterMarker.addWatermarkToImage(buffer, args)
+console.log('Request sent. Waiting for response...');
+
+await new Promise(resolve => setTimeout(resolve, 10000)); // Waits for 10 seconds
+
+client.close()
+console.log('Client closed.');
